79c9939c65243e5539b51d6b85eb0dc124e8607a
Anish Pokharel <anishpokharel@Anishs-MacBook-Air.local>
2026-04-26 17:37:03 +0545

    final

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api'
+  // apiUrl: 'http://localhost:3000/api'
+    apiUrl: 'https://kalbbackend-pnop.vercel.app/api', // Changed from localhost
+
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,8 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api', khalti: {
+  // apiUrl: 'http://localhost:3000/api'
+    apiUrl: 'https://kalbbackend-pnop.vercel.app/api', // Changed from localhost
+   khalti: {
     publicKey: 'live_public_key_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
     secretKey: 'live_secret_key_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
     returnUrl: 'https://your-domain.com/payment-callback'
